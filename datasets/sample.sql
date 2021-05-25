1c4489b7fc7a98585b72e6344af9db01b0d4fc50
Nadia <nadiasb91@gmail.com>
2021-05-25 14:30:05 -0300

    Add demo dataset
    Add demo dataset

---
+++ datasets/sample.sql
@@ -0,0 +1,28 @@
+BEGIN TRANSACTION;
+CREATE TABLE "Account" (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	"ParentId" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Account" VALUES(1,'Cuenta de ejemplo para asignaciones','');
+INSERT INTO "Account" VALUES(2,'Test account','');
+CREATE TABLE "Delivery_Item__c" (
+	id INTEGER NOT NULL, 
+	"Food_Expiration_Date__c" VARCHAR(255), 
+	"Food_Storage__c" VARCHAR(255), 
+	"Name" VARCHAR(255), 
+	"Delivery__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery_Item__c" VALUES(1,'2021-06-30','Refrigerated','Fruits','1');
+INSERT INTO "Delivery_Item__c" VALUES(2,'2021-06-28','Non-refrigerated','Vegetables','1');
+CREATE TABLE "Delivery__c" (
+	id INTEGER NOT NULL, 
+	"Scheduled_Date__c" VARCHAR(255), 
+	"Status__c" VARCHAR(255), 
+	"Supplier__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery__c" VALUES(1,'2021-05-27T15:00:00.000+0000','Scheduled','2');
+COMMIT;
