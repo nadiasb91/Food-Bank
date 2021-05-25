9dc0a2120efa426506eb050611126ba5142fb656
Nadia <nadiasb91@gmail.com>
2021-05-25 16:11:49 -0300

    Add data to sample.sql
    Add data to sample.sql

--- datasets/sample.sql
+++ datasets/sample.sql
@@ -6,7 +6,7 @@ CREATE TABLE "Account" (
 	PRIMARY KEY (id)
 );
 INSERT INTO "Account" VALUES(1,'Cuenta de ejemplo para asignaciones','');
-INSERT INTO "Account" VALUES(2,'Test account','');
+INSERT INTO "Account" VALUES(2,'asd','');
 CREATE TABLE "Delivery_Item__c" (
 	id INTEGER NOT NULL, 
 	"Food_Expiration_Date__c" VARCHAR(255), 
@@ -15,8 +15,8 @@ CREATE TABLE "Delivery_Item__c" (
 	"Delivery__c" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "Delivery_Item__c" VALUES(1,'2021-06-30','Refrigerated','Fruits','1');
-INSERT INTO "Delivery_Item__c" VALUES(2,'2021-06-28','Non-refrigerated','Vegetables','1');
+INSERT INTO "Delivery_Item__c" VALUES(1,'2021-05-29','Frozen','fruits','1');
+INSERT INTO "Delivery_Item__c" VALUES(2,'2021-05-29','Frozen','vegetables','1');
 CREATE TABLE "Delivery__c" (
 	id INTEGER NOT NULL, 
 	"Scheduled_Date__c" VARCHAR(255), 
@@ -24,5 +24,5 @@ CREATE TABLE "Delivery__c" (
 	"Supplier__c" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "Delivery__c" VALUES(1,'2021-05-27T15:00:00.000+0000','Scheduled','2');
+INSERT INTO "Delivery__c" VALUES(1,'2021-05-26T15:00:00.000+0000','Requested','2');
 COMMIT;
